3b445abe6b2f1510b64081a1ffde68a0d3f54578
Justin Ong <justin-ong@live.com.au>
2023-07-12 18:47:59 +1000

    Added music player - use-sound, +forward -backwards

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,9 +1,35 @@
-import { FaPlay } from "react-icons/fa";
+"use client";
+
+import usePlayer from "@/hooks/usePlayer";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import useGetSongById from "@/hooks/useGetSongbyId";
+
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
-  return (
-    <div>Player</div>
-  )
-}
+  const player = usePlayer();
+  const { song } = useGetSongById(player.activeId);
 
-export default Player
+  const songUrl = useLoadSongUrl(song!);
+
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
+
+  return (
+    <div
+      className="
+        fixed 
+        bottom-0 
+        bg-black 
+        w-full 
+        py-2 
+        h-[80px] 
+        px-4
+      ">
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
+    </div>
+  );
+};
+
+export default Player;
